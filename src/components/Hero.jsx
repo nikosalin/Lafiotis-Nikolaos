a27425d397045492dfb7992cd07a377829cee11d
elafios <elafios@arx.net>
2026-05-29 12:57:02 +0300

    Add profile picture and update import path in Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { HERO_CONTENT } from "../constants";
-import profilePic from "../assets/Pic.jpg";
+import profilePic from "../assets/Profile.jpg";
 import { motion } from "framer-motion";
 
 const container = (delay) => ({
