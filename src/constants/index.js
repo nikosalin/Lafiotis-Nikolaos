504959fb15909782c202fdd0c311ec79eb8079f6
elafios <joeyweler33@gmail.com>
2026-07-27 09:26:46 +0300

    qa

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,9 +4,9 @@ import project3 from "../assets/projects/project-3.jpg";
 import MovieTheater from "../assets/projects/MovieTheater.jpg";
 import Airbnb from "../assets/projects/Airbnb.jpg";
 
-export const HERO_CONTENT = `Hey there, Im Nikos and I am a web developer and linux Sysadmin.`;
+export const HERO_CONTENT = `Hey there, Im Nikos and I am a software engineer, a QA engineer and linux Sysadmin.`;
 
-export const ABOUT_TEXT = `I am a dedicated full stack developer with a passion for creating web applications. Since 2018 when I first started my coding courses through university, I have worked with a variety of technologies, including React, Next.js, Node.js, Java, Typescript, PostgreSQL, Firebase and MongoDB. My journey in web development began in university, and it has evolved into a lifetime passion where I continuously try to learn new things. I've also have plenty of knowlegde on System administration with linux. Outside of coding, I have good Project Managment knowledge on optical fiber.`;
+export const ABOUT_TEXT = `I am a dedicated full stack developer with a passion for creating web applications. Since 2018 when I first started my coding courses through university, I have worked with a variety of technologies, including React, Next.js, Node.js, Java, Typescript, PostgreSQL, Firebase, MongoDB and Playwright. My journey in web development began in university, and it has evolved into a lifetime passion where I continuously try to learn new things. I've also have plenty of knowlegde on System administration with linux. Outside of coding, I have good Project Managment knowledge on optical fiber.`;
 
 export const EXPERIENCES = [
   {
