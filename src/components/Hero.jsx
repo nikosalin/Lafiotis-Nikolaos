8dfea4a7ef1673fa65b666f04235015929882c1a
elafios <joeyweler33@gmail.com>
2026-08-25 15:47:13 +0300

    idiot

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -31,7 +31,7 @@ const Hero = () => {
               animate="visible"
               className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent"
             >
-              Full Stack Devolepr and Optical Fiber PM-FTTH
+              Full Stack Devoleper and Optical Fiber PM-FTTH
             </motion.span>
             <motion.p
               variants={container(1)}
